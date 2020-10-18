1d025fa942d52d9987fe3095df1ffbb382ea7e2b
yuktmitash <ymitash3@gatech.edu>
2020-10-18 06:26:27 -0400

    Updates

---
+++ precache-manifest.dbb5d801153e19dfc7e469d2c02941d6.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8eea686be651b38ee1e9a8d26aeb0d",
+    "url": "/HackNews/index.html"
+  },
+  {
+    "revision": "e399b3d469a5cf44f717",
+    "url": "/HackNews/static/css/2.cadfc43b.chunk.css"
+  },
+  {
+    "revision": "4165cd4c24aa3a615ebc",
+    "url": "/HackNews/static/css/main.9dd4a059.chunk.css"
+  },
+  {
+    "revision": "e399b3d469a5cf44f717",
+    "url": "/HackNews/static/js/2.ad845a60.chunk.js"
+  },
+  {
+    "revision": "079122604e96abf6fe643e6ac6359964",
+    "url": "/HackNews/static/js/2.ad845a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4165cd4c24aa3a615ebc",
+    "url": "/HackNews/static/js/main.6e36ff7a.chunk.js"
+  },
+  {
+    "revision": "ebe67988cdecd1305250",
+    "url": "/HackNews/static/js/runtime-main.f3e23770.js"
+  },
+  {
+    "revision": "9b9b1fe48e492e5c22c62f660e5ca17d",
+    "url": "/HackNews/static/media/Hack_News_Logo.9b9b1fe4.png"
+  },
+  {
+    "revision": "9772e15ed8114a3bc25878195ce60c5d",
+    "url": "/HackNews/static/media/Kevin.9772e15e.jpg"
+  },
+  {
+    "revision": "5e5f052ade9113d302dcc76b75481fae",
+    "url": "/HackNews/static/media/Michael.5e5f052a.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/HackNews/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/HackNews/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/HackNews/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/HackNews/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/HackNews/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/HackNews/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/HackNews/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/HackNews/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/HackNews/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/HackNews/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/HackNews/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/HackNews/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/HackNews/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/HackNews/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/HackNews/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/HackNews/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "48f3e2f4d36d6f724905cf5bf8caacc3",
+    "url": "/HackNews/static/media/peggy.48f3e2f4.jpg"
+  },
+  {
+    "revision": "c4544789798ae37ba55f4e440261b108",
+    "url": "/HackNews/static/media/textImage1.c4544789.jpg"
+  }
+]);
