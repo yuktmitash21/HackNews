442fdc43450c32ecf0dfe16f37cbee158b94d755
yuktmitash <ymitash3@gatech.edu>
2020-10-17 20:10:15 -0400

    kevins sstuff

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,19 +3,31 @@ import './stylesheet.scss';
 import {Image, Icon, Popup, Progress} from 'semantic-ui-react'
 import moment from 'moment';
 import { scaleLinear } from "d3-scale"
+import CommentSection from '../CommentSection/CommentSection';
 
 class Card extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            isUpvote: undefined
+            isUpvote: undefined,
+            cardStyle: {width: '100%'},
+            commentStyle: {width: '100%'}
         }
     }
 
     handleIconClick = (isUpvote) => {
         this.setState({isUpvote: isUpvote});
         this.props.handleVote(this.props.link, isUpvote);
+    };
+
+
+    hoverInAnimation = () => {
+        this.setState({...this.state, cardStyle: {width: '200%'}, commentStyle: {width: '0%'}});
+    };
+
+    hoverOutAnimation = () => {
+        this.setState({...this.state, cardStyle: {width: '100%'}, commentStyle: {width: '100%'}});
     };
 
     componentDidUpdate(prevProps, prevState, snapshot) {
@@ -26,6 +38,7 @@ class Card extends Component {
         let color = customScale(this.props.percent);
 
         let card = document.getElementById(this.props.link);
+        if (!card) return;
         let bar = card.getElementsByClassName('bar')[0];
         bar.style.background = color;
     }
@@ -38,6 +51,7 @@ class Card extends Component {
         let color = customScale(this.props.percent);
 
         let card = document.getElementById(this.props.link);
+        if (!card) return;
         let bar = card.getElementsByClassName('bar')[0];
         bar.style.background = color;
     }
@@ -46,35 +60,71 @@ class Card extends Component {
         const {title, description, image, pubDate, link, upvotes, downvotes, percent} = this.props;
         const {isUpvote} = this.state;
 
-
-
         return (
-            <div data-id={title} id={link} className="Card">
-                <Progress percent={percent} progress color="red" >Accuracy of News</Progress>
-                <Popup
-                    trigger={<Icon style={isUpvote !== undefined && isUpvote ? {backgroundColor: 'palevioletred'} : {}} onClick={() => this.handleIconClick(true)} className="down-icon" size='large' name="chevron up"/>}
-                    content={upvotes + ' Realvotes'}
-                    inverted
-                />
-                <br/>
-                <br/>
-                <Popup
-                    trigger={ <Icon style={isUpvote !== undefined && !isUpvote ? {backgroundColor: 'palevioletred'} : {}} onClick={() => this.handleIconClick(false)}  className="up-icon" size='large' name="chevron down"/>}
-                    content={downvotes + ' Fakevotes'}
-                    inverted
-                />
-                <h3 className="title">
-                    {title}
-                </h3>
-                <span className="text">{moment(new Date(pubDate)).fromNow()}</span>
-                <p className="text">
-                    <img className="img" src={image}/>
-                    {description}
-                </p>
-                <a href={link}/>
-
+            <div className = "WholeNews">
+                <div data-id={title} id={link} className="Card" style = {this.state.cardStyle} onMouseEnter={this.hoverInAnimation} onMouseLeave={this.hoverOutAnimation}>
+                    <Progress percent={percent} progress color="red" >Accuracy of News</Progress>
+                    <Popup
+                        trigger={<Icon style={isUpvote !== undefined && isUpvote ? {backgroundColor: 'palevioletred'} : {}} onClick={() => this.handleIconClick(true)} className="down-icon" size='large' name="chevron up"/>}
+                        content={upvotes + ' Realvotes'}
+                        inverted
+                    />
+                    <br/>
+                    <br/>
+                    <Popup
+                        trigger={ <Icon style={isUpvote !== undefined && !isUpvote ? {backgroundColor: 'palevioletred'} : {}} onClick={() => this.handleIconClick(false)}  className="up-icon" size='large' name="chevron down"/>}
+                        content={downvotes + ' Fakevotes'}
+                        inverted
+                    />
+                    <h3 className="title">
+                        {title}
+                    </h3>
+                    <span className="text">{moment(new Date(pubDate)).fromNow()}</span>
+                    <p className="text">
+                        <img className="img" src={image}/>
+                        {description}
+                    </p>
+                    <a href={link}/>
+                </div>
+                <div className="CommentSection" style = {this.state.commentStyle}>
+                    <CommentSection />
+                </div>
             </div>
         );
+
+
+
+        // return (
+        //     <div className = "WholeNews">
+        //         <div data-id={title} id={link} className="Card" style = {this.state.cardStyle} onMouseEnter={this.hoverInAnimation} onMouseLeave={this.hoverOutAnimation}>
+                    {/*<Progress percent={percent} classprogress color="red" >Accuracy of News</Progress>*/}
+                    {/*<Popup*/}
+                    {/*    trigger={<Icon style={isUpvote !== undefined && isUpvote ? {backgroundColor: 'palevioletred'} : {}} onClick={() => this.handleIconClick(true)} className="down-icon" size='large' name="chevron up"/>}*/}
+                    {/*    content={upvotes + ' Realvotes'}*/}
+                    {/*    inverted*/}
+                    {/*/>*/}
+                    {/*<br/>*/}
+                    {/*<br/>*/}
+                    {/*<Popup*/}
+                    {/*    trigger={ <Icon style={isUpvote !== undefined && !isUpvote ? {backgroundColor: 'palevioletred'} : {}} onClick={() => this.handleIconClick(false)}  className="up-icon" size='large' name="chevron down"/>}*/}
+                    {/*    content={downvotes + ' Fakevotes'}*/}
+                    {/*    inverted*/}
+                    {/*/>*/}
+                    {/*<h3 className="title">*/}
+                    {/*    {title}*/}
+                    {/*</h3>*/}
+        //             <span className="text">{moment(new Date(pubDate)).fromNow()}</span>
+        //             <p className="text">
+        //                 <img className="img" src={image}/>
+        //                 {description}
+        //             </p>
+        //             <a href={link}/>
+        //             <div className="CommentSection" style = {this.state.commentStyle}>
+        //                 <CommentSection />
+        //             </div>
+        //         </div>
+        //     </div>
+        // );
     }
 
 }
